66c45ad7d4a90499320feed7fda0531123ff1f44
Narendra Singh Khati <168346557+NAREN03052002@users.noreply.github.com>
2026-03-16 10:08:11 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # run.py
+import os
 from app import create_app, db
 from app.models import User, Course, Feedback
 
@@ -6,7 +7,13 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Course': Course, 'Feedback': Feedback}
+    return {
+        'db': db,
+        'User': User,
+        'Course': Course,
+        'Feedback': Feedback
+    }
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
